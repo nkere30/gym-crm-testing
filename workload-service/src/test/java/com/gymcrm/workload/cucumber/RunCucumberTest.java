6fde64471b9e51e1d8adb58304ea9b154678129c
Nino Kereselidze <ninokereselidze30@gmail.com>
2025-09-23 22:24:29 +0400

    Add Cucumber JUnit runners for CRM & Workload with HTML reports

--- workload-service/src/test/java/com/gymcrm/workload/cucumber/RunCucumberTest.java
+++ workload-service/src/test/java/com/gymcrm/workload/cucumber/RunCucumberTest.java
@@ -9,6 +9,6 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, summary, html:target/cucumber-report-workload.html")
 public class RunCucumberTest {
 }

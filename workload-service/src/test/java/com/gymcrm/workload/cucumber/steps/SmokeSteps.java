5f5e967239dab71183dbf32fc49db5dffd78a6d0
Nino Kereselidze <ninokereselidze30@gmail.com>
2025-09-24 00:46:40 +0400

    Add smoke step definitions for CRM and Workload. (For testing purposes, will be deleted later on)

---
+++ workload-service/src/test/java/com/gymcrm/workload/cucumber/steps/SmokeSteps.java
@@ -0,0 +1,12 @@
+package com.gymcrm.workload.cucumber.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class SmokeSteps {
+    @Given("nothing")
+    public void nothing() {}
+
+    @Then("nothing happens")
+    public void nothing_happens() {}
+}

fce0ea8e0d1e3e8ef8c80c8a0e3f1f8711b336e1
Nino Kereselidze <ninokereselidze30@gmail.com>
2025-09-24 07:36:05 +0400

    Configure test YAMLs; Mock Jms in CRM; Configure Workload test messaging props (queue & DLQ) for embedded ActiveMQ

--- gym-crm-service/src/test/java/com/gymcrm/cucumber/CucumberSpringConfiguration.java
+++ gym-crm-service/src/test/java/com/gymcrm/cucumber/CucumberSpringConfiguration.java
@@ -2,10 +2,14 @@ package com.gymcrm.cucumber;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberSpringConfiguration {
+    @MockBean
+    private JmsTemplate jmsTemplate;
 }

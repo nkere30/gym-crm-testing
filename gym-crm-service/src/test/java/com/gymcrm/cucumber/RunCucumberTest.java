f3a24a1df92b35c564d509d282d1dc166018a887
Nino Kereselidze <ninokereselidze30@gmail.com>
2025-09-28 21:43:35 +0400

    Disable JMS listeners in test profile to avoid VM broker errors

--- gym-crm-service/src/test/java/com/gymcrm/cucumber/RunCucumberTest.java
+++ gym-crm-service/src/test/java/com/gymcrm/cucumber/RunCucumberTest.java
@@ -10,8 +10,6 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-//@SelectClasspathResource("features/component/training_component.feature")
-//@SelectClasspathResource("features/component/user_component.feature")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, summary, html:target/cucumber-report-crm.html")
 public class RunCucumberTest {
 }
